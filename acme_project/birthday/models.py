a0d3a6220007de15a8a71805324c91933d69b539
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-06-24 22:12:01 +0300

    Add a custom validator for dates: Acme web and admin zone rejoice.

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from .validators import real_age
+
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
@@ -9,4 +11,11 @@ class Birthday(models.Model):
         help_text='Необязательное поле',
         max_length=20
     )
-    birthday = models.DateField('Дата рождения')
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+
+    class Meta:
+        verbose_name = 'день рождения'
+        verbose_name_plural = 'Дни рождения'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
