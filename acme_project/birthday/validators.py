7595875ebb9ffaa71bf046f2da5895344da76588
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-06-25 20:17:38 +0300

    Increase the max age limit to feature some artists' works as pictures.

--- acme_project/birthday/validators.py
+++ acme_project/birthday/validators.py
@@ -5,5 +5,5 @@ from django.core.exceptions import ValidationError
 
 def real_age(value: date) -> None:
     age = (date.today() - value).days / 365
-    if age < 1 or age > 120:
+    if age < 1 or age > 220:  # For the artists' sake
         raise ValidationError('Ожидается возраст от 1 года до 120 лет')
