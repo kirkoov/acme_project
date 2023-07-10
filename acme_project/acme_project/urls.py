48596c4c12f5fc5cba95f6388e88fa3f017df27b
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-07-10 07:56:18 +0300

    Registration CBV added via the main urls, as_view & the stand. user creation form.

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,7 +1,9 @@
 from django.conf import settings  # For dev only to share statics, pix, etc.
 from django.conf.urls.static import static  # Ditto
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import include, path, reverse_lazy
+from django.views.generic.edit import CreateView
 
 
 urlpatterns = [
@@ -9,5 +11,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # The plus part is for dev only to share statics, pix, etc. (DEBUG=True!)
